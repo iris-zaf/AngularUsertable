d0f601510b87f544b27f03333404e7849a4aae67
Iris <iriskalogirou1@gmail.com>
2024-10-31 23:25:56 +0200

    fixes on css

--- usertable/src/app/app.component.ts
+++ usertable/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostsComponent],
+  imports: [RouterOutlet, PostsComponent,MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
